6bab066d6a2edd6731e769b00675cbfeb5eeee5c
Alexandro <arvitaly@list.ru>
2017-04-03 18:20:13 +0700

    change type

--- typings.d.ts
+++ typings.d.ts
@@ -5,14 +5,15 @@ declare namespace PageGrabber {
     interface IModel {
         on: (name: string, cb: (data: any) => any) => void
     }
-    export function sel(selector: string, obj: any): GrabberO;
+    export function sel<T extends { [index: string]: any }>(selector: string, obj: T): T;
+    export function sel<T extends Array<{ [index: string]: any }>>(selector: string, obj: T): T;
     export function child(index: number, el: any): GrabberO;
-    export function text(): GrabberO;
-    export function val(): GrabberO;
-    export function attr(name: string): GrabberO;
-    export function css(name: string): GrabberO;
-    export function hasClass(name: string): GrabberO;
-    export function obj(path: string, obj?: any): GrabberO;
+    export function text(): string;
+    export function val(): string;
+    export function attr(name: string): string;
+    export function css(name: string): string;
+    export function hasClass(name: string): boolean;
+    export function obj<T extends { [index: string]: any }>(path: string, obj?: T): T;
 }
 declare function PageGrabber(window: Window): {
     observe: (obj: PageGrabber.GrabberO, onNewData: (data: any) => any, convert?: Function) => void;
